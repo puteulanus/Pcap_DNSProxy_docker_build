88db2f8256f0425b33f0c7941c412cb1ed865da8
Puteulanus <mj@puteulanus.com>
2016-01-18 23:36:32 +0800

    减少log

--- script/gcc-build.sh
+++ script/gcc-build.sh
@@ -6,8 +6,8 @@ version=5.3.0
 yum -y install gcc gcc-c++ make libmpc-devel
 
 cd /usr/src/
-wget -O gcc.tar.gz "http://ftp.mirrorservice.org/sites/sourceware.org/pub/gcc/releases/gcc-${version}/gcc-${version}.tar.gz"
-tar -zxvf gcc.tar.gz
+wget -qO gcc.tar.gz "http://ftp.mirrorservice.org/sites/sourceware.org/pub/gcc/releases/gcc-${version}/gcc-${version}.tar.gz"
+tar -zxf gcc.tar.gz
 cd gcc-${version}
 ./configure --disable-multilib --enable-languages=c,c++
 make -j 4
