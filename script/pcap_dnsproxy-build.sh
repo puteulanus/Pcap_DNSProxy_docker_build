071bfb7475f1070f8c931b496552a1f55d33c194
Puteulanus <mj@puteulanus.com>
2016-01-20 00:53:23 +0800

    启用静态编译

--- script/pcap_dnsproxy-build.sh
+++ script/pcap_dnsproxy-build.sh
@@ -5,6 +5,6 @@ cd /usr/src/
 wget -qO pcapdns.zip "https://github.com/chengr28/Pcap_DNSProxy/archive/master.zip"
 tar -zxf pcapdns.zip
 cd Pcap_DNSProxy-master/Source/Scripts
-bash ./Linux_Build.sh
+bash ./Linux_Build.sh --enable-static
 
 # Configure
